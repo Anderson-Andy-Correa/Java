5e781140d60148ef6943288365cd95d92d2e6861
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2023-07-17 13:22:20 -0300

    Criação do Projeto Loja e aplicação com Hibernate

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProdutp.java
@@ -0,0 +1,30 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.Util.JPAUtil;
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProdutp {
+    public static void main(String[] args) {
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal",
+                new BigDecimal("800"), celulares);
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
+        em.getTransaction().begin();
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
