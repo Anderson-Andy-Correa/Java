d66ac28b25b2f8ad656197e1f408e6f545185824
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2023-07-18 22:10:26 -0300

    Finalização do primeiro curso de Hibernate

---
+++ Alura/2 - Java com Banco de dados/2 - Presistencia com JPA - Hibernate/Projeto/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutp.java
@@ -0,0 +1,47 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.Util.JPAUtil;
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CadastroDeProdutp {
+    public static void main(String[] args) {
+        //cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto produto = produtoDao.buscarPorId(1l);
+        System.out.println(produto.getPreco());
+
+        List<Produto> lista = produtoDao.buscarPorNomeCategoria("CELULAREs");
+        lista.forEach(p -> System.out.println(p.getCategoria().getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        System.out.println("Preco do Produto: " + precoDoProduto);
+        em.close();
+    }
+
+    private static void cadastrarProduto() {
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal",
+                new BigDecimal("800"), celulares);
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
+        em.getTransaction().begin();
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
